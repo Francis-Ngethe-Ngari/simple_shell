19a62b3968183e6a9ef30a093348e922d7202527
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2023-11-16 02:50:06 +0300

    fixed betty doc style by adhering to guidelines

--- 0-strcmp.c
+++ 0-strcmp.c
@@ -1,12 +1,14 @@
 # include "shell.h"
 
 /**
- * _strcmp - 
+ * _strcmp - Compares difference of two strings until find
+ * the difference or reaches the end of both strings.
  *
- * @str_1:
- * @str_2:
+ * @str_1: Pointer to 1st string.
+ * @str_2: Pointer to 2nd string
  *
- * Return:
+ * Return: 0 if both strings are equal or a positive or negative
+ * value based on the difference.
  *
  */
 
@@ -16,6 +18,8 @@ int _strcmp(char *str_1, char *str_2)
 	{
 		if (*str_1 != *str_2)
 			return (*str_1 - *str_2);
+		*str_1++;
+		*str_2++;
 	}
 	if (*str_1 != '\0')
 		return (1);
