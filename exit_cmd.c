78adf5159e312b13889439b5f9890ce8c30076a7
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2023-11-16 17:01:04 +0300

    Modified exit_cmd function to use sh command to handle exiting operation

--- exit_cmd.c
+++ exit_cmd.c
@@ -10,5 +10,22 @@
  */
 void exit_cmd(void)
 {
-	exit(0);
+	/*The shell command to handle the exit of the program*/
+	const char *sh_cmd = "sh -c 'echo Program is exiting; exit 0'";
+	/*Use the system function to execute the shell command*/
+	int status = system(sh_cmd);
+
+	if (status == -1)
+		perror("Error executing shell command");
+	else
+	{
+		if (WIFEXITED(status))
+		{
+			int ext_status = WEXITSTATUS(status);
+
+			printf("Shell command exited with status %d\n", ext_status);
+		}
+		else
+			printf("Shell command did not exit normally\n");
+	}
 }
