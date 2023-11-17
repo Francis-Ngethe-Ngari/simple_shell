8448a9efec5a49702c21cdcdfce8853e94b3a88b
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2023-11-17 03:01:09 +0300

    updated whileloop to be able to iterate through environ variable while printing env variables

--- print_env.c
+++ print_env.c
@@ -11,12 +11,13 @@
  */
 void print_env(void)
 {
-	char **env = environ;
+	extern char **environ;
+	int itr = 0;
 
-	while (*env != NULL)
+	while (environ[itr] != NULL)
 	{
-		write(STDOUT_FILENO, *env, _strlen(*env));
+		write(STDOUT_FILENO, environ[itr], _strlen(environ[itr]));
 		write(STDOUT_FILENO, "\n", 1);
-		env++;
+		itr++;
 	}
 }
